fd82f5ef3aeec76874e5a69be977ed16a5b18e64
Sunkist18 <dandalf@o.cnu.ac.kr>
2022-01-18 22:25:42 +0900

    GitIgnore 설정

--- main.py
+++
@@ -1,8 +0,0 @@
-from majwaeteul.judge import *
-
-problem = Problem(1000)
-problem.getRandomArray(100, 1, 100)
-size = 10
-print(problem.getRandomArrayPair(size, 1, 4, 1, 5, True, pair_type=1))
-print(problem.getRandomArrayPair(size, 1, 4, 1, 5, False, pair_type=1))
-problem.Run()
